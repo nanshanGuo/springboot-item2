1b196a05d4542307f03a4971011dd55ffa11ec6c
422496509@qq.com <422496509@qq.com>
2021-07-30 17:06:55 +0900

    Initial commit

---
+++ src/main/java/com/nanshan/NanshanApplication.java
@@ -0,0 +1,11 @@
+package com.nanshan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NanshanApplication {
+    public static void main(String[] args){
+        SpringApplication.run(NanshanApplication.class,args);
+    }
+}
