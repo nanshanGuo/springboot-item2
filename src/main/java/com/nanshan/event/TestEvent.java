35637543ab654b257c07c3cf90e89e5c514821c3
422496509@qq.com <422496509@qq.com>
2021-08-06 18:01:07 +0900

    aaa

---
+++ src/main/java/com/nanshan/event/TestEvent.java
@@ -0,0 +1,22 @@
+package com.nanshan.event;
+
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.ContextStoppedEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TestEvent {
+
+    @EventListener
+    public void handleApplicationReady(ApplicationReadyEvent applicationReadyEvent) {
+        System.out.println("起動完了後、該当メソッドを呼び出される");
+    }
+
+    @EventListener
+    public void handleContextStopped(ContextStoppedEvent contextStoppedEvent) {
+        System.out.println("停了停了停了停了停了停了停了停了停了停了停了停了停了停了停了停了");
+    }
+
+
+}
